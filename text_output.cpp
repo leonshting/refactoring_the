c0f7b0997e85e2aed2f1017974dd4182b0919a46
leonshting <leonshting@gmail.com>
2016-03-11 17:06:26 +0300

    done_partly3

--- text_output.cpp
+++ text_output.cpp
@@ -13,6 +13,19 @@ VectorXcd text_output::cAnswer_fromD(VectorXd &answer) {
     int csize = answer.rows()/2;
     VectorXd ret(csize);
     for(int i = 0 ; i < csize; i++)
-       ret(i) = cd(answer(i), answer(csize+i));
+       ret(i) = complex<double>(answer(i), answer(csize+i));
     return ret;
+}
+
+void text_output::make_polynoms() {
+    map<string, data_points<data_point_with_azimuth>>::iterator i;
+    for(i = initData.data_points_collection.begin(); i!=initData.data_points_collection.end(); ++i)
+    {
+        tag_to_cpolynom.insert({i->second.tag, });
+    }
+}
+
+string text_output::make_polynom(data_points<data_point_with_azimuth> &subj) {
+    string ret;
+
 }
--- text_output.h
+++ text_output.h
@@ -2,10 +2,14 @@
 // Created by leonsht on 3/10/16.
 //
 #include <eigen3/Eigen/Dense>
+#include <map>
+#include <string>
 
 #include "settings.h"
 #include "init_data.h"
 #include "useful_linear_algebra.h"
+#include "data_points.h"
+
 
 #ifndef STRESS_REC_REFACTORED_TEXT_OUTPUT_H
 #define STRESS_REC_REFACTORED_TEXT_OUTPUT_H
@@ -24,6 +28,9 @@ private:
     settings Settings;
     init_data initData;
     VectorXcd cAnswer_fromD(VectorXd &answer);
+    map<string, string> tag_to_cpolynom;
+    void make_polynoms();
+    string make_polynom(data_points<data_point_with_azimuth> &subj);
 };
 
 #endif //STRESS_REC_REFACTORED_TEXT_OUTPUT_H
